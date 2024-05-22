5bbc1334b92a36222288fb1a4b8069bb1539b0eb
Derstood <1831003848@qq.com>
2024-05-23 00:25:38 +0800

    Create getSceneByVar.js

---
+++ getSceneByVar.js
@@ -0,0 +1,36 @@
+var aimVarNamePattern = new RegExp("模式");
+
+await (async () => {
+    // 定义 callAPI 函数，用于调用 API 并返回一个 Promise
+    const callAPI = (api, params) => {
+        return new Promise(res => editor.gateway.callAPI(api, params, res));
+    };
+
+    // 调用 getDevList API 获取设备列表
+    const devList = (await callAPI('getDevList')).devList;
+
+    // 调用 getGraphList API 获取规则列表
+    const ruleList = await callAPI('getGraphList');
+
+    // 初始化设备规则映射对象
+    let varRuleMap = {};
+
+    const varList = await callAPI('getVarList', { scope: 'global'});
+    const name_id__map = Object.fromEntries(Object.entries(varList).map(([key, value]) => [value.userData.name, key]));
+    // 遍历每个规则
+    for (const rule of ruleList) {
+        // 调用 getGraph API 获取规则的详细信息
+        const content = await callAPI('getGraph', { id: rule.id }); 
+        for (const n of content.nodes) {
+            if (n.props?.scope === "global" ) {
+                varName = varList[n.props?.id].userData.name
+                if ( aimVarNamePattern.test(varName) ) {
+                    varRuleMap[varName] = varRuleMap[varName] ?? [];
+                    varRuleMap[varName].push(rule.userData.name);
+                    break; // 满足条件后退出当前 for...of 循环
+                }
+            }
+        }
+    }
+    return varRuleMap
+})();
